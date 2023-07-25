cf3a9a1f56c6cce19535a475f36b9baff1f68b5f
lesponns <lesponns@DESKTOP-44MF2L6>
2023-07-25 18:19:42 +0300

    Proje dosyası ekle.

---
+++ SmartPro_8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPro_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Heyy, Selam ben Adal!");
+            Console.ReadKey();
+        }
+    }
+}
