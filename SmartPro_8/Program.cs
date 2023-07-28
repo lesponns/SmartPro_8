f2b9fe1bd98a00297eabab5a57f184175ab21ec0
lesponns <71814927+lesponns@users.noreply.github.com>
2023-07-28 07:10:26 +0300

    Update Program.cs

--- SmartPro_8/Program.cs
+++ SmartPro_8/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +6,105 @@ using System.Threading.Tasks;
 
 namespace SmartPro_8
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Heyy, Selam ben Adal!");
+            double num1, num2, last;
+            string calc;
+
+            Console.WriteLine("Yapacağınız işlemi seçin!");
+            Console.WriteLine("");
+            Console.WriteLine("Toplama    [+]");
+            Console.WriteLine("Çıkarma    [-]");
+            Console.WriteLine("Çarpma     [*]");
+            Console.WriteLine("Bölme      [/]");
+            Console.WriteLine("Ortalama   [ort]");
+            Console.WriteLine("");
+            Console.Write("Seçilen işlem: ");
+            calc = Console.ReadLine();
+
+            if (calc == "+")
+            {
+                Console.Clear();
+                Console.Write("İlk Sayıyı giriniz: ");
+                num1 = double.Parse(Console.ReadLine());
+
+                Console.Clear();
+                Console.Write("İkinci sayıyı giriniz: ");
+                num2 = double.Parse(Console.ReadLine());
+
+                last = num1 + num2;
+
+                Console.Clear();
+                Console.Write("İşlem: " + num1 + " + " + num2 + " = " + last);
+              
+            }
+            else if (calc == "-")
+            {   
+                Console.Clear();
+                Console.Write("İlk Sayıyı giriniz: ");
+                num1 = double.Parse(Console.ReadLine());
+
+                Console.Clear();
+                Console.Write("İkinci sayıyı giriniz: ");
+                num2 = double.Parse(Console.ReadLine());
+
+                last = num1 - num2;
+
+                Console.Clear();
+                Console.Write("İşlem: "+ num1 +" - "+num2+" = "+ last);
+            }
+            else if (calc == "*")
+            {   
+                Console.Clear();
+                Console.Write("İlk Sayıyı giriniz: ");
+                num1 = double.Parse(Console.ReadLine());
+
+                Console.Clear();
+                Console.Write("İkinci sayıyı giriniz: ");
+                num2 = double.Parse(Console.ReadLine());
+
+                last = num1 * num2;
+
+                Console.Clear();
+                Console.Write("İşlem: "+ num1 +" * "+num2+" = "+ last);
+            }
+            else if (calc == "/")
+            {   
+                Console.Clear();
+                Console.Write("İlk Sayıyı giriniz: ");
+                num1 = double.Parse(Console.ReadLine());
+
+                Console.Clear();
+                Console.Write("İkinci sayıyı giriniz: ");
+                num2 = double.Parse(Console.ReadLine());
+
+                last = num1 / num2;
+
+                Console.Clear();
+                Console.Write("İşlem: "+ num1 +" / "+num2+" = "+ last);
+            }
+            else if (calc == "ort")
+            {
+                Console.Clear();
+                Console.Write("İlk Sayıyı giriniz: ");
+                num1 = double.Parse(Console.ReadLine());
+
+                Console.Clear();
+                Console.Write("İkinci sayıyı giriniz: ");
+                num2 = double.Parse(Console.ReadLine());
+
+                last = (num1 + num2) / 2;
+
+                Console.Clear();
+                Console.Write("İşlem: " + num1 + " + " + num2 + " / 2 = " + last);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Hatalı işlem!");
+            }
             Console.ReadKey();
         }
     }
